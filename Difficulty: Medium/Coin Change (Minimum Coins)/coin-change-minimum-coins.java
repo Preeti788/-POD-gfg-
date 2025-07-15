e1d67a7e748230905eb9680c9eba4005319b31e8
Preeti788 <kumaripreetispj@gmail.com>
2025-07-16 00:32:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int minCoins(int coins[], int sum) {
+        // code here
+        int[] dp = new int[sum +1];
+        Arrays.fill(dp, sum+1);
+        dp[0] = 0;
+        
+        for(int i=1; i<=sum; i++){
+            for(int coin : coins){
+                if(coin <= i){
+                    dp[i] = Math.min(dp[i], dp[i-coin] +1);
+                }
+            }
+        }
+        
+        return dp[sum] > sum ? -1 : dp[sum];
+        
+    }
+}
