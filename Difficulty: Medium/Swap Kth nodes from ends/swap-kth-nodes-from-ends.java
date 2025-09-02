e2cae91eb5ff581377d50b040f31b67f443ff6a7
Preeti788 <kumaripreetispj@gmail.com>
2025-09-02 21:52:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.java
@@ -0,0 +1,46 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node swapKth(Node head, int k) {
+        // code here
+        if (head == null) return null;
+
+        int n = 0;
+        Node temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+
+        if (k > n) return head;
+        if (2 * k - 1 == n) return head;
+
+        Node x = head;
+        for (int i = 1; i < k; i++) {
+            x = x.next;
+        }
+
+        Node y = head;
+        for (int i = 1; i < n - k + 1; i++) {
+            y = y.next;
+        }
+
+        int t = x.data;
+        x.data = y.data;
+        y.data = t;
+
+        return head;
+
+
+        
+    }
+}
