e9eb223891ab0d1b14ae5445f809b03c272d70cf
Preeti788 <kumaripreetispj@gmail.com>
2025-09-05 22:47:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -0,0 +1,41 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public Node segregate(Node head) {
+        // code here
+        if (head == null || head.next == null) return head;
+        
+        int[] count = new int[3];
+        Node curr = head;
+        
+        while (curr != null){
+            count[curr.data]++;
+            curr = curr.next;
+        }
+        
+        curr = head;
+        int i = 0;
+        
+        while(curr != null){
+            if(count[i] == 0){
+                i++;
+            }else{
+                curr.data = i;
+                count[i]--;
+                curr = curr.next;
+            }
+        }
+        
+        return head;
+    }
+}
